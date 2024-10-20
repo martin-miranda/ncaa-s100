70cbe8689ab4cea06120138e2d651311c0e8bd59
Martin Miranda <carl_miranda@dlsu.edu.ph>
2024-10-20 17:02:49 +0800

    As of Oct 20

--- display_colored.py
+++ display_colored.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
-from st_aggrid import AgGrid
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 pd.options.display.float_format = "{:,.2f}".format
@@ -27,11 +26,11 @@ with open('team_colors.txt','r') as file:
 with open('title.txt', 'r') as file:
     title = [line.strip() for line in file]
 st.title(title[0])
-st.markdown('By Martin Miranda @mc_miranda34')
-st.markdown('Raw Box Scores from Pong Ducanes: ncaa.livestats.ph')
-st.markdown('#### As of Game 55: JRU vs. SSCR (Oct. 16)')
+st.markdown('By Carl Miranda @mc_miranda34')
+st.markdown('Raw Box Scores from Pong Ducanes: uaap.livestats.ph')
+st.markdown('#### As of Game 61 - MU vs. SBU (Oct. 19)')
 
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['Player Per-Game Stats', 'Player Per-30 Stats', 'Player Advanced Stats', 'Team Per-Game Stats', 'Team Advanced Stats', 'Glossary'])
+tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(['Player Per-Game Stats', 'Player Per-30 Minute Stats', 'Player Advanced Stats', 'Team Per-Game Stats', 'Opponent Per-Game Stats', 'Team Advanced Stats', 'Glossary'])
 cm = sns.dark_palette("green", as_cmap=True)
 
 with tab1:
@@ -69,7 +68,8 @@ with tab2:
         st.write(df)
 
 with tab3:
-    st.markdown('*Please note the discrepancies on the SP calculation at the bottom of the page.*')
+    st.markdown('*Note for SP: Penalties are not included due to inavailability of data.*')
+    st.markdown('*Note for WS: Values are re-normalized every after game. This should not affect rankings.*')
     st.header('All Players', divider='gray')
     df = pd.read_csv('advanced_stats.csv', index_col=['PLAYER','TEAM'])
     df = df[(df['MPG'] * df['GP']) >= df['GP'].max() * 8]
@@ -86,12 +86,6 @@ with tab3:
         df = df.style.background_gradient(cmap=tcm, axis=0).format("{:.2f}")
         st.write(df)
 
-    st.write('---')
-    st.markdown('There are slight discrepancies on the official SP calculations due to the bonus and penalties in the formula. This would not probably affect **rankings** unless adjacent players have very close SP.')
-    st.markdown('The discrepancies are as follows:')
-    st.markdown('1. Win bonuses are counted by team instead of by player for simplicity in coding. *For example, the missed win by JD Cagulangan is still included in his bonus.*')
-    st.markdown('2. Penalties are not included at all due to lack of availability on technical and unsportsmanlike foul data.')
-
 with tab4:
     st.header('All Teams', divider='gray')
     df = pd.read_csv('team_per_game.csv', index_col=['TEAM'])
@@ -102,12 +96,20 @@ with tab4:
 
 with tab5:
     st.header('All Teams', divider='gray')
+    df = pd.read_csv('opp_per_game.csv', index_col=['TEAM'])
+    df = df.reindex(columns=tb_cols)
+    df = df.drop(labels=['W','L','W%','MINS'], axis=1)
+    df = df.style.background_gradient(cmap=cm, axis=0).format("{:.2f}")
+    st.write(df)
+
+with tab6:
+    st.header('All Teams', divider='gray')
     df = pd.read_csv('team_advanced.csv', index_col=['TEAM'])
     df = df.reindex(columns=ta_cols)
     df = df.style.background_gradient(cmap=cm, axis=0).format("{:.2f}")
     st.write(df)
 
-with tab6:
+with tab7:
     with open('Glossary.md','r') as f:
         markdown_content = f.read()
         st.markdown(markdown_content)
